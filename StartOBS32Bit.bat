cbba4bfc4e817ff8254a91fd28947d986a2b31be
Benjamin Meißner <benjamin.meissner@stud.htwk-leipzig.de>
2017-05-04 17:34:43 +0200

    added AutoHotkeyScripts to Git

---
+++ StartOBS32Bit.bat
@@ -0,0 +1,5 @@
+cd bin/32bit
+start obs32.exe
+cd ../../Grafiken/
+start Info.txt
+exit 0
---
+++ StartOBS64Bit.bat
@@ -0,0 +1,7 @@
+cd bin/64bit
+start obs64.exe
+cd ../../Grafiken/
+start Info.txt
+cd ../AutoHotkey
+start obs.ahk
+exit 0
