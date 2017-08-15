4049fc60f90dd69b28233dc669eb6c2cb5888202
Benjamin Meißner <benjamin.meissner@stud.htwk-leipzig.de>
2017-08-16 01:53:00 +0200

    Added new/fixed existing scripts

--- updateOverlay.bat
+++ updateOverlay.bat
@@ -1,1 +1,1 @@
- start "" "/PortableGit/bin/sh.exe" -c "git fetch --all && git reset --hard origin/master"
+ start "Update LESeV OBS" "PortableGit/bin/sh.exe" -c "git fetch --all && git reset --hard origin/master"
