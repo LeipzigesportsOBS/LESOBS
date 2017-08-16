d54afc51141fd4dcf405d0047db19be584b10df9
Benjamin Meißner <benjamin.meissner@stud.htwk-leipzig.de>
2017-08-16 14:47:53 +0200

    Added Font Installation, Nicer Bat files

--- Install/Windows-64/download.bat
+++
@@ -1,6 +0,0 @@
-::wget\wget.exe -O obs.zip https://github.com/jp9000/obs-studio/releases/download/20.0.1/OBS-Studio-20.0.1-Full.zip
-::unzip\bin\unzip.exe obs.zip
-wget\wget.exe -O git.7z.exe https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/PortableGit-2.14.1-64-bit.7z.exe
-echo Bitte den Anweisungen des sich öffnenden Programms folgen und im aktuellen Ordner entpacken!
-timeout /t 5
-start git.7z.exe
--- Install/Windows-64/install.bat
+++ Install/Windows-64/install.bat
@@ -1,16 +1,54 @@
 @echo off
+echo ------------------------------------------------------------------------------------------------
+echo  Download & Entpacken der OBS Binaries
+echo ------------------------------------------------------------------------------------------------
+echo
+timeout /t 3
 wget\wget.exe -O obs.zip https://github.com/jp9000/obs-studio/releases/download/20.0.1/OBS-Studio-20.0.1-Full.zip
 unzip\bin\unzip.exe obs.zip
 rm obs.zip
+echo
+echo ------------------------------------------------------------------------------------------------
+echo  Download der GIT Binaries
+echo ------------------------------------------------------------------------------------------------
+echo
+timeout /t 3
 wget\wget.exe -O git.7z.exe https://github.com/git-for-windows/git/releases/download/v2.14.1.windows.1/PortableGit-2.14.1-64-bit.7z.exe
-echo Bitte den Anweisungen des sich oeffnenden Programms folgen und im aktuellen Ordner entpacken!
+echo
+echo ------------------------------------------------------------------------------------------------
+echo  Bitte den Anweisungen des sich oeffnenden Programms folgen und im aktuellen Ordner entpacken!
+echo ------------------------------------------------------------------------------------------------
+echo
 timeout /t 5
 start /max /wait git.7z.exe
 rm git.7z.exe
+echo
+echo ------------------------------------------------------------------------------------------------
+echo  Overlay wird nun heruntergeladen und Installiert!
+echo ------------------------------------------------------------------------------------------------
+echo
+timeout /t 3
 start /wait "" "PortableGit/bin/sh.exe" -c "git clone https://github.com/LeipzigesportsOBS/LESOBS/ tmp && mv tmp/.git . && rm -rf tmp && git reset --hard"
-echo die Installation ist abgeschlossen!
+echo 
+echo ------------------------------------------------------------------------------------------------
+echo  Im naechsten Schrit werden die benoetigten Schriftarten installiert                        
+echo  In den  4 oeffneden Fenstern bitte Installieren klicken und diese im Anschluss schliessen      
+echo ------------------------------------------------------------------------------------------------
+echo
+timeout /t 8
+cd fonts
+start "" /wait installfont.bat
+cd ..
+echo 
+echo ------------------------------------------------------------------------------------------------
+echo  die Installation ist abgeschlossen!
+echo  OBS wird in kürze gestartet!
+echo  fuer schnellen Zugrif eine Verknuepfung zur StartOBS64Bit.bat manuell anlegen!
+echo ------------------------------------------------------------------------------------------------
+echo
 rmdir /S wget
 rmdir /S unzip
+rmdir /S fonts
 pause
 start StartOBS64Bit.bat
 echo del %%~dpnx0 ^>nul>%temp%\CleanUp.Bat 
