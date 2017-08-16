8f45020b9ef2db22d0614183e40b1241b6d356e5
LeipzigesportsOBS <31048467+LeipzigesportsOBS@users.noreply.github.com>
2017-08-16 02:01:12 +0200

    Update EigeneSzeneVorschlagen.bat
    Fixed problem with pushing

--- GitCommands/EigeneSzeneVorschlagen.bat
+++ GitCommands/EigeneSzeneVorschlagen.bat
@@ -2,5 +2,5 @@
 set /p szene=Bitte namen der Szene eingeben (Leerzeichen durch _ ersetzen!): 
 set szene=%szene%.json
 set /p branch=Bitte deinen Namen und Spiel eingeben (Ohne Leerzeichen!):  
-start "../" /D "../" "../PortableGit/bin/sh.exe" -c "git stash && git checkout -b %branch% && git stash pop &&  git commit -m 'MeineSzene' config/obs-studio/basic/scenes/%szene% && git push --set-upstream origin %branch% && git checkout master"
+start "../" /D "../" "../PortableGit/bin/sh.exe" -c "git stash && git checkout -b %branch% && git stash pop && git add . &&  git commit -m 'MeineSzene' config/obs-studio/basic/scenes/%szene% && git push --set-upstream origin %branch% && git checkout master"
 pause
